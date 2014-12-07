47fb971702fac9e75cb27c54f8a532af61b7608c
includelife <262548467@qq.com>
2014-12-07 16:36:32 +0800

    Revert "more simple"
    This reverts commit dd551fe53ecf17579321f4b7d30220f859b62111.

--- src/playcard/Main.java
+++ src/playcard/Main.java
@@ -3,12 +3,11 @@ package playcard;
 import java.awt.Button;
 import java.awt.FlowLayout;
 
-import frame.LoginFrame;
-
 public class Main {
 
 	public static void main(String args[]) {
-		LoginFrame login = new LoginFrame();
-		login.setVisible(true);
+		MainFrame mypane = new MainFrame();
+		mypane.init();
+		mypane.setVisible(true);
 	}
 }
