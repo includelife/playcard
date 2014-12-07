57a06980144bd95ee1bdb1bf4ad4de05868fa88a
includelife <262548467@qq.com>
2014-12-07 16:36:59 +0800

    a new package
    en?

---
+++ src/frame/LoginFrame.java
@@ -0,0 +1,139 @@
+package frame;
+
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+//import frame.JProgressBarFrame;
+
+public class LoginFrame extends JFrame implements ActionListener{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	
+	private JTextField nameTF;
+	private JPasswordField passwordTF;
+	private JButton loginButton = null, resetButton = null;
+	private JProgressBarFrame progressBar = null;
+	private String username = null, password = null;
+	
+	public LoginFrame() {
+		// TODO Auto-generated constructor stub
+		super();
+		getContentPane().setLayout(null);
+		
+		jFrameValidate(); 
+		
+		setTitle("登录游戏");
+		
+		JLabel backgroundLabel = new JLabel();
+		backgroundLabel.setBounds(0, 0, 800, 600);
+		backgroundLabel.setLayout(null);
+		
+		
+		final JLabel nameLable = new JLabel();
+		nameLable.setText("用户名 ：");
+		nameLable.setBounds(230, 203, 100, 18);
+		//将服务器标签添加进背景标签
+		backgroundLabel.add(nameLable);
+		
+		final JLabel passLable = new JLabel();
+		passLable.setText("密  码 ：");
+		passLable.setBounds(230, 243, 100, 18);
+		//将服务器标签添加进背景标签
+		backgroundLabel.add(passLable);
+		
+		nameTF = new JTextField();
+		nameTF.setBounds(370, 203, 150, 22);
+		backgroundLabel.add(nameTF);
+		
+		//密码
+		passwordTF = new JPasswordField();
+		passwordTF.setBounds(370, 243, 150, 22);
+		backgroundLabel.add(passwordTF);
+		
+		loginButton = new JButton("登录");
+		resetButton = new JButton("重置");
+		backgroundLabel.add(loginButton);
+		backgroundLabel.add(resetButton);
+		loginButton.setBounds(280, 360, 80, 30);
+		resetButton.setBounds(400, 360, 80, 30);
+		
+		//添加监听器
+		loginButton.addActionListener(this);
+		resetButton.addActionListener(this);
+		
+		//把背景标签添加进内容面板
+		getContentPane().add(backgroundLabel);
+		
+		progressBar = new JProgressBarFrame(this, "登录", "登录中，请稍后...");
+		
+		reset();// 默认初始值
+	}
+	
+	private void reset() {
+		// TODO Auto-generated method stub
+		nameTF.setText("");
+		passwordTF.setText("");
+	}
+
+	private void jFrameValidate() {
+		// TODO Auto-generated method stub
+		// 获得系统的相关属性，这里需要屏幕的宽和高
+		Toolkit tk = getToolkit();
+		//Dimension是Java的一个类，封装了一个构件的高度和宽度
+		Dimension dim = tk.getScreenSize();
+		this.setResizable(false);
+		//框架居中显示  760 540
+		this.setBounds(dim.width / 2 - 400, dim.height / 2 - 300, 800, 600);
+		validate();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == loginButton) {// 登录
+			progressBar.setVisible(true);// 设置进度条可见
+			new Thread() {
+				public void run() {
+//					getValues();// 得到界面中的所有项的值
+					checkUser();// 登录验证
+				}
+			}.start();
+		} else if (e.getSource() == resetButton) {// 重置
+			reset();// 重新设置各项的值
+		}
+	}
+
+	protected void checkUser() {
+		// TODO Auto-generated method stub
+		progressBar.dispose();
+		this.dispose();
+		MainFrame myframe = new MainFrame();
+		myframe.init();
+		myframe.initSelect();
+		myframe.setVisible(true);
+		
+	}
+
+	@SuppressWarnings("deprecation")
+	protected void getValues() {
+		// TODO Auto-generated method stub
+		username = nameTF.getText().trim();
+		password = passwordTF.getText().trim();
+	}
+
+}
---
+++ src/frame/MainFrame.java
@@ -0,0 +1,126 @@
+package frame;
+
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+public class MainFrame extends JFrame implements ActionListener{
+	private Container pane = null;
+	private JButton single = null;
+	private JButton multi = null;
+	private JButton help = null;
+	private JButton langue = null;
+	
+	public MainFrame() {		
+		super();
+	}
+	
+	public void init() {
+		this.setTitle("斗地主");
+		this.setSize(800, 600);
+		setResizable(false);
+		setLocationRelativeTo(getOwner()); // 屏幕居中
+		pane = this.getContentPane();
+		pane.setLayout(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pane.setBackground(new Color(0, 112, 26)); // 背景为绿色		
+		
+		
+//		JLabel backgroundLabel = new JLabel();
+//		backgroundLabel.setBounds(0, 0, 800, 600);
+//		backgroundLabel.setLayout(null);
+//		
+//		single = new JButton("单人模式");
+//		multi = new JButton("多人模式");
+//		help = new JButton("帮  助");
+//		langue = new JButton("语  言");
+//		
+//		single.setBounds(365, 190, 100, 40);
+//		multi.setBounds(365, 190+50, 100, 40);
+//		help.setBounds(365, 190+50*2, 100, 40);
+//		langue.setBounds(365, 190+50*3, 100, 40);
+//		
+//		single.addActionListener(this);
+//		multi.addActionListener(this);
+//		help.addActionListener(this);
+//		langue.addActionListener(this);
+//
+//		backgroundLabel.add(single);
+//		backgroundLabel.add(multi);
+//		backgroundLabel.add(help);
+//		backgroundLabel.add(langue);
+//
+//		this.getContentPane().add(backgroundLabel);
+	}	
+	
+	public void initSelect(){
+		JLabel backgroundLabel = new JLabel();
+		backgroundLabel.setBounds(0, 0, 800, 600);
+		backgroundLabel.setLayout(null);
+		
+		single = new JButton("单人模式");
+		multi = new JButton("多人模式");
+		help = new JButton("帮  助");
+		langue = new JButton("语  言");
+		
+		single.setBounds(365, 190, 100, 40);
+		multi.setBounds(365, 190+50, 100, 40);
+		help.setBounds(365, 190+50*2, 100, 40);
+		langue.setBounds(365, 190+50*3, 100, 40);
+		
+		single.addActionListener(this);
+		multi.addActionListener(this);
+		help.addActionListener(this);
+		langue.addActionListener(this);
+
+		backgroundLabel.add(single);
+		backgroundLabel.add(multi);
+		backgroundLabel.add(help);
+		backgroundLabel.add(langue);
+
+		this.getContentPane().add(backgroundLabel);
+	}
+	
+	public void init3() {
+		JButton returnbutton = new JButton("返 回");
+		JButton settingbutton = new JButton("设 置");
+		
+		returnbutton.setBounds(700, 10, 80, 20);
+		settingbutton.setBounds(700, 40, 80, 20);
+		
+		this.getContentPane().add(returnbutton);
+		this.getContentPane().add(settingbutton);
+		
+		
+	}
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == single){
+			//JOptionPane.showMessageDialog(this, "单人模式");
+			this.dispose();	
+			MainFrame fr = new MainFrame();
+			fr.init();
+			fr.init3();
+			fr.setVisible(true);
+		}else if(e.getSource() == multi){
+			JOptionPane.showMessageDialog(this, "开发中");
+		}else if(e.getSource() == help){
+			JOptionPane.showMessageDialog(this, "您需要什么帮助？");
+		}else if(e.getSource() == langue){
+			JOptionPane.showMessageDialog(this, "开发中");	
+		}
+	}
+}
